a4bbbb2ea541d336db7988877c57b48f0adaeab3
galeano <jhongaleanomartin@gmail.com>
2026-04-20 22:10:30 -0500

    feat: se agrego encriptacion de contraseñas y el metodo post en usuarios

--- src/main/java/tareas/demo/security/Security.java
+++ src/main/java/tareas/demo/security/Security.java
@@ -7,18 +7,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class Security {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
---
+++ src/main/java/tareas/demo/services/UsuarioService.java
@@ -0,0 +1,25 @@
+package tareas.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import tareas.demo.models.usuarios;
+import tareas.demo.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public usuarios guardarUsuario(usuarios usuario){
+        String passwordCifrada = passwordEncoder.encode(usuario.getContrasenna());
+
+        usuario.setContrasenna(passwordCifrada);
+
+        return usuarioRepository.save(usuario);
+    }
+}
