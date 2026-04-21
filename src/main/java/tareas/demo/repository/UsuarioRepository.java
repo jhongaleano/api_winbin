238713a383f8154ed8c8a7234d6fba939bea61d7
galeano <jhongaleanomartin@gmail.com>
2026-04-20 21:19:08 -0500

    feat: se agrega autorizacion para el login del usuario

--- src/main/java/tareas/demo/repository/UsuarioRepository.java
+++ src/main/java/tareas/demo/repository/UsuarioRepository.java
@@ -3,7 +3,9 @@ package tareas.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tareas.demo.models.usuarios;
+import java.util.*;
+
 
 public interface UsuarioRepository extends JpaRepository<usuarios, Integer> {
-    
+    Optional<usuarios> findByDocumento(Integer documento);
 }
