4bfef5c0b9fe22a593bc41618467523c952d114c
cristhianpa22 <cristhianp.soft@gmail.com>
2026-06-06 18:10:31 -0500

    feat: se agrego el modelo, controlador y repositorio de la tabla registroIa

---
+++ src/main/java/tareas/demo/repository/RegistroIaRepository.java
@@ -0,0 +1,8 @@
+package tareas.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tareas.demo.models.RegistroIa;
+
+public interface RegistroIaRepository extends JpaRepository<RegistroIa, Integer> {
+
+}
